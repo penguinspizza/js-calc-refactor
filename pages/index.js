011d857b0855c79c134968e98a7917a72aaab740
penguinspizza <72650078+penguinspizza@users.noreply.github.com>
2021-11-20 13:30:51 +0900

    Move components and add next export to package.json

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Layout from "../components/layout"
-import Calc from "../components/calc"
+import Layout from "./components/layout"
+import Calc from "./components/calc"
 
 export default function Home() {
   return (
     <div>
-      <Layout header="JavaScript Calculator v1.1.1" title="js-calc" footer="&copy; penguinspizza.">
+      <Layout header="JavaScript Calculator" title="js-calc" footer="&copy; penguinspizza.">
         <Calc />
       </Layout>
     </div>
