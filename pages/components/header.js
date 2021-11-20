d39fde66461cf06a1e98d29b98cce7c7948f4062
penguinspizza <72650078+penguinspizza@users.noreply.github.com>
2021-11-20 13:56:45 +0900

    Without Image from next/image

--- pages/components/header.js
+++ pages/components/header.js
@@ -1,11 +1,11 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 
 export default function Header(props) {
   return (
     <div>
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand d-flex align-items-center" href="#">
-          <Image src="/favicon.ico" width="30" height="30" className="d-inline-block" alt="icon" />
+          {/* <Image src="/favicon.ico" width="30" height="30" className="d-inline-block" alt="icon" /> */}
           <span className="pl-3 pr-3">{props.header}</span>
         </a>
       </nav>
